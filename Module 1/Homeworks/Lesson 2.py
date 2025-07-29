9ce1d658060a711b1bba54a51043981658649f53
sahibdeep205 <sahibdeep.kalyan@arcadiaschool.ae>
2025-07-29 14:18:38 +0400

    submit

---
+++ Module 1/Homeworks/Lesson 2.py
@@ -0,0 +1,13 @@
+# Creating different varibles
+x = 5
+y = "John"
+print (x)
+print (y)
+
+codingal = "codingal"
+print(codingal)
+
+# User Input
+Sahib = input ("Sahib: ")
+
+print ("\nHello", Sahib, "\nwelcome to codingal")
