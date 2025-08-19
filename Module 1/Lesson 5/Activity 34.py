02a32075461509e83d23ebbc0d111e8cbb8b2e90
sahibdeep205 <sahibdeep.kalyan@arcadiaschool.ae>
2025-08-19 14:08:33 +0400

    submit

---
+++ Module 1/Lesson 5/Activity 34.py
@@ -0,0 +1,8 @@
+i = int(input("enter a number : 11"))
+if (i < 15):
+    print ("i is smaller than")
+    print ("i'm in if Block")
+else:
+    print ("i is greater than 15")
+    print ("i'm in else Blcok")
+print (" i'm not in fi and not in else Block")
