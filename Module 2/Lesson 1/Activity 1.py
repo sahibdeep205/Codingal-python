858b8e88948ca098c1bc0684fa22d3b159550f26
sahibdeep205 <sahibdeep.kalyan@arcadiaschool.ae>
2025-08-20 14:15:50 +0400

    Submit

---
+++ Module 2/Lesson 1/Activity 1.py
@@ -0,0 +1,22 @@
+#Python program to illustrate the use 
+# of 'is' identify operator
+x = 5
+if (type(x) is int):
+    print("true")
+else:
+    print("false")
+
+x = 5.0
+if (type(x) is not float):
+    print("true")
+else:
+    print("false")
+
+
+x = 20
+y = 20
+if (x is y):
+       print("x & y SAME identity")
+y=30
+if (x is not y):
+       print("x & y have DIFFERENT identity")    
